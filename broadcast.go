b56362b2877c5c7083ff9d3cc5cce32f7f681edf
Arpith Siromoney <arpith@feedreader.co>
2015-05-25 14:29:18 +0530

    broadcast now uses redigo and manucoporat/sse

--- broadcast.go
+++ broadcast.go
@@ -1,66 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"sync"
-	"time"
-
-	"github.com/sudhirj/strobe"
+	"github.com/manucorporat/sse"
+	"github.com/garyburd/redigo/redis"
 )
 
-type broker struct {
-	channels map[string]*strobe.Strobe
-	sync.RWMutex
-}
-
-func (b *broker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	channelName := r.URL.Path
-	b.Lock()
-	channel, ok := b.channels[channelName]
-	if !ok {
-		channel = strobe.NewStrobe()
-		b.channels[channelName] = channel
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+    		// handle error
 	}
-	b.Unlock()
+	defer c.Close()
+	psc := redis.PubSubConn{c}
 	switch r.Method {
 	case "GET":
-		f, ok := w.(http.Flusher)
-		if !ok {
-			http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
-			return
-		}
-
-		closer, ok := w.(http.CloseNotifier)
-		if !ok {
-			http.Error(w, "Closing unsupported!", http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "keep-alive")
-
-		listener := channel.Listen()
-		defer channel.Off(listener)
+		psc.Subscribe(channelName)
 		for {
-			select {
-			case msg := <-listener:
-				fmt.Fprintf(w, "data: %s\n\n", msg)
-				f.Flush()
-			case <-closer.CloseNotify():
-				return
-			case <-time.After(300 * time.Second):
-				return
+			switch v := psc.Receive().(type) {
+			case redis.Message:
+				sse.Encode(w, sse.Event{
+     					Event: "message",
+					Data:  v.Data,
+				})
+			case error:
+				// handle error
 			}
 		}
+	
 	case "POST":
-		go channel.Pulse("PING")
+		r.ParseForm()
+		savedChannelName, err = redis.String(c.Do("GET", r.Form["token"]))
+		if err != nil {
+			// handle error
+		} else if savedChannelName != channelName {
+			// handle authentication error
+		} else {
+			c.Do("PUBLISH", channelName, r.Form["message"])
+		}
 	}
+
 }
 
 // NewBroadcastHandler creates a new handler that handles pub sub
 func NewBroadcastHandler() func(w http.ResponseWriter, req *http.Request) {
-	broker := &broker{channels: make(map[string]*strobe.Strobe)}
-	return broker.ServeHTTP
+	return ServeHTTP
 }
